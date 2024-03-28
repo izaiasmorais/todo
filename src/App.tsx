aa7125e0f4aa87c3b9a146693673ee6afabe2553
Izaias Lima <izaiaslima356@gmail.com>
2024-03-28 18:18:36 -0300

    build: set up vite app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+	return (
+		<main className="w-full h-screen bg-zinc-800">
+			<div className="w-full h-[200px] bg-zinc-900 flex items-center justify-center">
+				<img src="./Logo.png" alt="Logo" />
+			</div>
+		</main>
+	);
+}
